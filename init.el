06f7eb4f01b2392e20cf6a814688bea146eefd38
PatrickHoward <patrickmhoward@icloud.com>
2024-04-23 16:12:16 -0400

    Add tabs to default config

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
+ ;; Your init file should contain only one such instance.nnnnnnnnnnn
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(misterioso)))
 (custom-set-faces
@@ -11,9 +11,17 @@
  ;; If there is more than one, they won't work right.
  )
 
+(tab-bar-mode t)
 (setq inhibit-startup-screen t)
 
-(set-face-attribute 'default nil :height 105)
+;; set transparency
+(set-frame-parameter nil 'alpha-background 98)
+(add-to-list 'default-frame-alist '(alpha-background . 98))
+
+;;(set-frame-parameter (selected-frame) 'alpha '(90 100))
+;;(add-to-list 'default-frame-alist '(alpha 90 100))
+
+(set-face-attribute 'default nil :height 125)
 
 ;; Hide the UI, unless we're on MacOS since the global menu is already out of the way
 (tool-bar-mode 0)
@@ -32,13 +40,12 @@
 (if (eq system-type 'darwin)
     (setq shell-file-name "/bin/zsh")
   (if (eq system-type 'windows-nt)
-      (setq shell-file-name "C:/Program Files/nu/bin/nu.exe"))
-  )
+      (setq shell-file-name "C:/Program Files/nu/bin/nu.exe")))
 
 (setq inferior-lisp-program "sbcl")
 
-(setq auto-save-file-name-transforms
-      `((".*" ,(concat user-emacs-directory "auto-save/") t)))
+(setq auto-save-file-name-transforms 
+  `(("\\`/[^/]*:\\([^/]*/\\)*\\([^/]*\\)\\'" ,(concat user-emacs-directory "auto-save/" "\\2") t)))
 
 ;;(setq default-directory (concat (getenv "HOME") (get-system-slash)))
 (setq default-directory "~/")
@@ -46,3 +53,4 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
+
